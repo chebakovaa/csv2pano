31830e6b0b14ad692d69e42b4463eccf6ffd21cc
ChebakovAA <achbkv@gmail.com>
2021-04-02 16:06:41 +0500

    add attributes for relation

--- src/main/java/com/bisoft/App.java
+++ src/main/java/com/bisoft/App.java
@@ -75,9 +75,9 @@ public class App
                         String val = "";
                         s = String.format("LOAD CSV WITH HEADERS FROM 'file:///pitc/%s.csv' AS row WITH row WHERE row.%s IS NOT NULL", entity, fields[0]);
                         String pars = Arrays.stream(fields).map(v -> String.format("%1$s: row.%1$s", v)).collect(Collectors.joining(","));
-                        pars = pars.replace(String.format("row.%s", fields[0])
-                          , String.format("toInteger(row.%s)", fields[0]));
-                        s += String.format(" MERGE (o:%s {%s}); ", entity, pars);
+//                        pars = pars.replace(String.format("row.%s", fields[0])
+//                          , String.format("row.%s", fields[0]));
+                        s += String.format(" MERGE (o:%1$s {%2$s, oname: '%1$s', otype: 'item'}); ", entity, pars);
                         Result result = tx.run(s);
                         return result.toString();
                     }
@@ -101,7 +101,8 @@ public class App
                         String toE = entity.split("_")[2];
                         String fn = entity;
                         s = String.format("LOAD CSV WITH HEADERS FROM 'file:///pitc/%s.csv' AS row", fn);
-                        s += String.format(" MATCH (e:%1$s {%3$s: toInteger(row.from_id)}), (o:%2$s {%3$s: toInteger(row.to_id)}) CREATE (e)-[r:CONTAINED_INTO]->(o)"
+                        s += String.format(" MATCH (e:%1$s {%3$s: row.from_id}), (o:%2$s {%3$s: row.to_id}) " +
+                            "CREATE (e)-[r:CONTAINED_INTO {uid: '%1$s-' + o.uid, oname: '%1$s', otype: 'folder'}]->(o)"
                           , fromE, toE, fields[0]);
                         Result result = tx.run(s);
                         return result.toString();
