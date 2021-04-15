2e51200c6713c99ea4eb8c276a075cb527aa1151
ChebakovAA <achbkv@gmail.com>
2021-04-15 11:53:24 +0500

    add indexes

--- src/main/java/com/bisoft/App.java
+++ src/main/java/com/bisoft/App.java
@@ -41,7 +41,7 @@ public class App
         emptyNeo(driver);
         loadTableData(driver, models);
         loadRelationData(driver, relations);
-        loadTimeEventData(driver, folder, events);
+//        loadTimeEventData(driver, folder, events);
         loadDimData(driver, folder, dims);
         driver.close();
     }
@@ -204,18 +204,34 @@ public class App
                 String s = "";
                 s = String.format("LOAD CSV WITH HEADERS FROM 'file:///pitc/%s.csv' AS row FIELDTERMINATOR ';' WITH row WHERE row.%s IS NOT NULL", file, fields[0]);
                 String pars = Arrays.stream(fields).map(v -> String.format("%1$s: row.%1$s", v)).collect(Collectors.joining(","));
-                s += String.format(" MERGE (o:%1$s {%2$s, oname: '%1$s', otype: 'item'}); ", entity, pars);
-                //session.run(s);
-    
+                s += String.format(" MERGE (o:%1$s {%2$s, oname: '%1$s', otype: 'item'}) return count(o); ", entity, pars);
                 final String query = s;
+    
+                
+                session.writeTransaction(tx -> {
+                    Result result = tx.run(
+                      String.format("DROP INDEX %1$s_pk_uid_unique IF EXISTS", entity)
+                    );
+                    return result.toString();
+                });
+                
                 String greeting = session.writeTransaction(tx -> {
                     Result result = tx.run(query);
                     return result.toString();
                 });
+
+                session.writeTransaction(tx -> {
+                    Result result = tx.run(
+                      String.format("create constraint %1$s_pk_uid_unique IF NOT EXISTS on (n:%1$s) assert n.uid is unique", entity)
+                    );
+                    return result.toString();
+                });
+
                 System.out.println( " pass" );
             }
         }
     }
+
     
     private static void loadRelationData(Driver driver, String[] entities) {
         try ( org.neo4j.driver.Session session = driver.session() )
